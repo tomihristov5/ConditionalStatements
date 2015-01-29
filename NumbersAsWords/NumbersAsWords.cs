42039c710db641d551e062441affc788243d8582
tomihristov5 <hrist0v@abv.bg>
2015-01-29 12:57:43 +0200

    update

--- NumbersAsWords/NumbersAsWords.cs
+++ NumbersAsWords/NumbersAsWords.cs
@@ -7,7 +7,7 @@ class NumbersAsWords
 {
     static void Main()
     {
-        string[] zeroTo19 = new string[] { "Zero", "One", "Two", "three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fiftheen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        string[] zeroTo19 = new string[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fiftheen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
         string[] twenties = new string[] { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
         Console.Write("Enter an integer from 0 to 999: ");
         int number = int.Parse(Console.ReadLine());
@@ -19,24 +19,24 @@ class NumbersAsWords
         {
             if (number % 10 == 0)
             {
-                Console.WriteLine(twenties[(number / 10) - 2]);
+                Console.WriteLine("The number as word is: " + twenties[(number / 10) - 2]);
             }
             else
-            Console.WriteLine(twenties[(int)(number / 10) - 2] + " " + zeroTo19[number % 10]);
+                Console.WriteLine("The number as word is: " + twenties[(int)(number / 10) - 2] + " " + zeroTo19[number % 10]);
         }
         if (number >= 100 && number <= 999)
         {
             if (number % 100 == 0)
             {
-                Console.WriteLine(zeroTo19[number / 100] + " hundred");
+                Console.WriteLine("The number as word is: " + zeroTo19[number / 100] + " hundred");
             }
             else
                 if (number % 10 == 0)
                 {
-                    Console.WriteLine(zeroTo19[(int)(number / 100)] + " hundred and " + twenties[(number / 10) % 10 - 2]);
+                    Console.WriteLine("The number as word is: " + zeroTo19[(int)(number / 100)] + " hundred and " + twenties[(number / 10) % 10 - 2]);
                 }
                 else
-                    Console.WriteLine(zeroTo19[(int)(number / 100)] + " hundred and " + twenties[(number / 10) % 10 - 2] + " " + zeroTo19[(number % 100) % 10]);
+                    Console.WriteLine("The number as word is: " + zeroTo19[(int)(number / 100)] + " hundred and " + twenties[(number / 10) % 10 - 2] + " " + zeroTo19[(number % 100) % 10]);
         }
         else
             Console.WriteLine("The number is out of range!");
